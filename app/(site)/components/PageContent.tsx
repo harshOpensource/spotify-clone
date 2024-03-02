4300e9d7f38adb07e407a45450498e84dc339a28
harshOpensource <harshopensource@gmail.com>
2024-03-02 12:49:10 +0530

    final_build fix

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -10,11 +10,12 @@ interface Props {
 }
 
 function PageContent({ songs }: Props) {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No Songs Available!</div>;
   }
 
-  const onPlay = useOnPlay(songs);
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4 ">
       {songs.map((song) => (
